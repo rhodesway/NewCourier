b5e8f0e236152b06e65084935bd7e27a26e98208
Rhodesway <rhodeswaycourier@outlook.com>
2026-06-28 15:47:30 -0700

    Adding auth

---
+++ Backend/models/Parcel.js
@@ -0,0 +1,22 @@
+const monogose = require("mongoose")
+
+const ParcelSchema = monogose.Schema({
+
+    from: { type: String, require: true },
+    to: { type: String, require: true },
+    sendername: { type: String, require: true },
+    recipientname: { type: String, require: true },
+    senderemail: { type: String, require: true },
+    recipientemail: { type: String, require: true },
+    weight: { type: Number, require: true },
+    cost: { type: Number, require: true },
+    date: { type: String, require: true },
+    note: { type: String },
+    feedback: { type: String },
+    status: { type: Number, default: 0 },
+
+}, {
+    timestamp: true
+})
+
+module.exports = monogose.model("Parcel", ParcelSchema)
---
+++ Backend/models/User.js
@@ -0,0 +1,17 @@
+const monogose = require("mongoose")
+
+const UserSchema = monogose.Schema({
+
+    fullname: { type: String, require: true },
+    email: { type: String, require: true },
+    age: { type: Number },
+    country: { type: String, require: true },
+    address: { type: String, require: true },
+    password: { type: String, require: true },
+    status: { type: Number, default: 0 },
+    role: { type: String, default: user },
+}, {
+    timestamp: true
+})
+
+module.exports = monogose.model("User", UserSchema)
